49bda0413ea2325a1e5a741bd22f87065ff55d17
hector.munoz <hector.munoz@mxmart.mx>
2023-01-10 17:06:25 -0600

    Finished sidebar

--- app/src/components/Navigation/Sidebar/Sidebar.jsx
+++ app/src/components/Navigation/Sidebar/Sidebar.jsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { AiOutlineSend } from "react-icons/ai";
 import "./Sidebar.css";
 
 const Sidebar = ({ isActive, toggleSideBar }) => {
   return (
-    <aside className={`sidebar ${isActive ? `active` : ``}`}>
+    <aside
+      className={`sidebar bg-slate-900 text-white ${isActive ? `active` : ``}`}
+    >
       <nav className="menu ">
-        <NavLink to="/" className="menu-item">
-          <i className="fas fa-fw fa-tachometer-alt"></i>{" "}
-          <span className="">Dashboard</span>
-        </NavLink>
-        <hr className="c-sidebar-divider" />
         <div className="heading">
-          <p>Item 1</p>
+          <p>Menu</p>
         </div>
         <NavLink to="/accounts" className="menu-item">
-          <i className="fas fa-users"></i> <span>Sub item 1</span>
+          <AiOutlineSend className="text-white" color="white" />{" "}
+          <span>Home</span>
         </NavLink>
         <NavLink to="/companies" className="menu-item">
-          <i className="fas fa-building"></i> <span>Sub item 2</span>
+          <AiOutlineSend /> <span>Sended invitations</span>
         </NavLink>
-        <NavLink to="/graduates" className="menu-item">
-          <i className="fas fa-user-graduate"></i> <span>Sub item 3</span>
-        </NavLink>
-        <hr className="c-sidebar-divider" />
       </nav>
     </aside>
   );
