60ba6a721ebe47af2937a441c6c08396e4a75b74
hector.munoz <hector.munoz@mxmart.mx>
2023-01-11 22:58:54 -0600

    Trying more fixes

--- app/src/routes/index.jsx
+++ app/src/routes/index.jsx
@@ -2,8 +2,7 @@ import IsLoggedIn from "@/components/Authentication/IsLoggedIn";
 import authRoutes from "./auth";
 
 import { Home } from "@/pages/Home";
-
-import NotFound from "@/pages/Status/NotFound";
+import { Link } from "react-router-dom";
 
 const index = [
   {
@@ -17,7 +16,15 @@ const index = [
   //Not found route
   {
     path: "*",
-    element: <NotFound />,
+    element: (
+      <div className="w-full h-screen flex flex-col justify-center items-center gap-3 dark:text-white">
+        <h4 className="font-bold text-8xl animate-bounce">404</h4>
+        <p className="font-medium text-2xl">Oops, that didn't work</p>
+        <Link to={-1} className="underline">
+          Back
+        </Link>
+      </div>
+    ),
   },
 ];
 
