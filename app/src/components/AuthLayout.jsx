259e35b6dd48dd3b72a9ded06ceb7ed893dc840b
hector.munoz <hector.munoz@mxmart.mx>
2023-01-18 12:35:38 -0600

    Push to deploy

--- app/src/components/AuthLayout.jsx
+++ app/src/components/AuthLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AuthLayout = ({ children }) => {
   return (
-    <div className="w-11/12 sm:10/12 md:6/12 lg:w-3/12 mx-auto mt-16">
+    <div className="w-11/12 sm:10/12 md:6/12 lg:w-3/12 mx-auto mt-16 a">
       {children}
     </div>
   );
